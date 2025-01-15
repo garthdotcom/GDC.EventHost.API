27cfef0fba76fb61363fde008185a09bd24a289b
Garth Heuchert <garth@garthheuchert.com>
2025-01-15 09:14:18 +0000

    Trying to sync up the projects.

--- GDC.EventHost.DTO/Performance/PerformanceTicketCount.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GDC.EventHost.DTO.Performance
-{
-    public class PerformanceTicketCount
-    {
-        public required Guid PerformanceId { get; set; }
-        public required int TotalTickets { get; set; }
-        public required int RemainingTickets { get; set; }
-    }
-}
